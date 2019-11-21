7a7f47e9c1553fe4b39104821b2ce8976376215f
MaiFujisawa <57941202+MaiFujisawa@users.noreply.github.com>
2019-11-21 10:26:48 +0900

    Update total_number_of_words.py

--- total_number_of_words.py
+++ total_number_of_words.py
@@ -8,14 +8,12 @@ import collections
 m = MeCab.Tagger ("-Owakati")
 
 with open(file_name, 'r') as fp:
-
     sum = 0
     for line in fp:
         line = m.parse(line)
         result = line.split()
         print(result)
         result = len(result)
-        #resultは各メール中の単語の数
         sum += result
 
     try:
