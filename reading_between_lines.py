4a1d949d2df35b462132b42096ba14bdaeb570b6
MaiFujisawa <57941202+MaiFujisawa@users.noreply.github.com>
2019-12-03 16:17:00 +0900

    print_mails_between_mails
    if you give 2 number, this program show you that the mails between the lines which you gave.

--- reading_between_lines.py
+++ reading_between_lines.py
@@ -1,5 +1,5 @@
 # ファイル内の文書のn-m行目の内容を表示する
-#kadai4,5
+
 import sys
 file_name = sys.argv[1]
 line_start = sys.argv[2]
@@ -10,19 +10,24 @@ line_goal_num = int(line_goal) + 1
 
 
 def file_reading(fp):
-    for line in fp:
-        line = line.rstrip()
-        lines.append(line)
+    for mail in fp:
+        mail_str = mail.rstrip()
+        lines.append(mail_str)
 
 
-with open(file_name, 'r') as fp:
-    lines = []
-    file_reading(fp)
-    last_num = len(lines)
-    if ((line_start_num <= line_goal_num)and(line_goal_num<last_num)):
-        print(lines[line_start_num:line_goal_num])
-    elif ((line_start_num > line_goal_num)and(line_goal_num<last_num)):
-        print(lines[line_goal_num:])
-        print(lines[:line_start_num])
-    else:
-        print("error!")
+def main():
+    with open(file_name, 'r') as fp:
+        lines = []
+        file_reading(fp)
+        last_num = len(lines)
+        if ((line_start_num <= line_goal_num)and(line_goal_num<last_num)):
+            print(lines[line_start_num:line_goal_num])
+        elif ((line_start_num > line_goal_num)and(line_goal_num<last_num)):
+            print(lines[line_goal_num:])
+            print(lines[:line_start_num])
+        else:
+            print("error!")
+            
+            
+if __name__ == '__main__':
+    main()
