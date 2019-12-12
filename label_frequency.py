3ddd96065595c978f04aeafe98771fcaf3c874fe
MaiFujisawa <57941202+MaiFujisawa@users.noreply.github.com>
2019-12-12 12:25:22 +0900

    Update label_frequency.py

--- label_frequency.py
+++ label_frequency.py
@@ -1,39 +1,64 @@
-#comment
+# pre_naive_bayes program
+# halfway
+
+
 import sys
 import MeCab
-m = MeCab.Tagger("-Owakati")
+m = MeCab.Tagger('-Owakati')
 
 
-def file_reading(file_name):
-    lines = []
+def road_file(file_name):
     with open(file_name, 'r') as fp:
-        for line in fp:
-            line = line.rstrip('\n')
-            lines.append(line)
-    return lines
+        mails = []
+        for sentence in fp:
+            sentences = (sentence.rstrip()).split(',')
+            mails.append(sentences)
+    return mails
 
-def file_writing(dict):
-    with open("output.txt",'w') as fp:
-        fp.write(dict)
 
-def label_freq(sentence):
-    words = []
+def count_label2frequency(sentence):
+    # str->dictionary
+    sentence = wakati_sentence(sentence)
+    word2frequency = count_sentence2frequency(sentence)
+    return word2frequency
+
+
+def wakati_sentence(sentence):
+    # str->list
+    words = m.parse(sentence).split()
+    return words
+
+def count_sentence2frequency(sentence):
+    # str->dictionary
+    sentence2frequency = {}
     for sent in sentence:
-        sent = m.parse(sent).split(" ")
-        words.append(sent)
+        sentence2frequency[sent] = sentence2frequency.get(sent, 0) + 1
+    return sentence2frequency
 
-    label2frequency = {}
-    words = str(words)
-    for label in words:
-        label2frequency[label] = label2frequency.get(label, 0) + 1
 
-    items = label2frequency.items()
-    for label,freq in sorted(label2frequency.items(),key=lambda x: -x[1]):
-        print(str(label) + ":" + str(freq))
+def main(textfile_name):
+    mails = road_file(textfile_name)
+    number_of_mails = len(mails)
+    #print(mails[0][0]) N
+    #print(mails[0][1]) （重複して...ただくこともあります．
 
-def main():
-    file_name = sys.argv[1]
-    lines = file_reading(file_name)
-    label_freq(lines)
+    # make word2frequency per labels
+    # split label and sentence
 
-main()
+    labels = []
+    words_labels = []
+    mails_list = []
+    for mail in mails:
+        labels.append(mail[0])
+        for sentence in mail:
+            for label in labels:
+                words = wakati_sentence(sentence)
+                word2frequency = count_sentence2frequency(words)
+            word_label2freq = count_sentence2frequency(words_labels)
+    print(word2frequency/len(word_label2freq))
+
+
+if __name__ == '__main__':
+    textfile_name = sys.argv[1]
+    main(textfile_name)
+
