8505f9145824693f0f9b17b3e21ac03bcc680f6e
MaiFujisawa <57941202+MaiFujisawa@users.noreply.github.com>
2020-03-25 16:52:06 +0900

    make_word2freq

--- make_word2frequency.py
+++ make_word2frequency.py
@@ -23,25 +23,29 @@ def wakati_sentence(sentence):
 
 
 def count_string(string, sentence2frequency={}):
-    # str->dictionary
     for character in string:
         sentence2frequency[character] = sentence2frequency.get(character, 0) + 1
     return sentence2frequency
 
 
-def main(textfile_name):
-    mails = road_file(textfile_name)
-    number_of_mails = len(mails)
+def count_probability_of_labels(w2f,number_of_mails,word2probability={}):
+    for key in w2f:
+        word2probability[key] = word2probability.get(key, 0) / number_of_mails
+    return word2probability
 
-    for mail in mails:
-        for sentence in mail:
+
+def main(textfile_name):
+    labels_and_sentences = road_file(textfile_name)
+    number_of_mails = len(labels_and_sentences)
+
+    for label_and_sentence in labels_and_sentences:
+        for sentence in label_and_sentence:
             words = wakati_sentence(sentence)
             word2frequency = count_string(string=words)
-            for key in word2frequency:
-                word2frequency[key] = word2frequency.get(key, 0) / number_of_mails
             print(word2frequency)
+            prob_label = count_probability_of_labels(word2frequency,number_of_mails)
+            
 
 if __name__ == '__main__':
     textfile_name = sys.argv[1]
     main(textfile_name)
-
